b159dcab4f2be76108124b4c7fcf14ab84f6ed17
hironmoy-ray <hpray35@gmail.com>
2024-10-07 21:15:00 +0600

    Calculate winner by simple JS functions formula and Render the Result on UI

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,15 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
 
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner is: ${winner}`;
+  } else {
+    status = "Next Player is:" + " " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
     if (squares[i]) {
       return;
@@ -30,6 +39,7 @@ export default function Board() {
 
   return (
     <>
+      <div>{status}</div>
       <div className="flex">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,4 +57,24 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
